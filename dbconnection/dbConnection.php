60e428b288c587a97c907bdc131c9da8e2e97f5a
DrFace <praveen.liyanage@axcertro.com>
2025-06-30 14:57:41 +0530

    "Removed commented out database connection settings and PDO connection attempt."

--- dbconnection/dbConnection.php
+++ dbconnection/dbConnection.php
@@ -6,7 +6,13 @@
     $password = "";
     $database = "mms1810b";
    
-       
+
+    //$servername = "perahara.lk";
+    //$username = "kelum";
+    //$password = "sKy@1234";
+    //$database = "mms1810a";
+    //$database2 = "wms1760g_mcdata";
+    
     try 
     {
         $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
